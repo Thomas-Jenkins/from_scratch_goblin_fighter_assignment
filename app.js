88fa4a005b250c21a610a7dacdb53055b7b1fa98
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-08-04 15:48:59 -0700

    add functionality to bring more goblins into the fight

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 import { goblins } from './goblins-data.js';
 import { renderGoblin } from './render-utils.js';
 const goblinBox = document.getElementById('goblin-box');
+const addGoblin = document.querySelector('[name="add-goblins"]');
+const playercharacter = document.querySelector('#player-character');
 // let state
 
 // set event listeners 
@@ -15,4 +17,16 @@ function displayGoblins() {
         goblinBox.append(goblinDisEL);
     }
 }
+
 displayGoblins();
+
+addGoblin.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let var01 = Math.floor(Math.random() * 100);  
+    const newGoblin = {
+        name: `Goblin #${var01}`,
+        health: 3
+    };
+    goblins.push(newGoblin);
+    displayGoblins();
+});
