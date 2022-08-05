23dd248249e92e2f143fff5d77cc7218a02db2cd
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-08-05 09:51:23 -0700

    add combat log

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 // import functions and grab DOM elements
 import { goblins } from './goblins-data.js';
-// import { renderGoblin } from './render-utils.js';
-// import { findGoblin } from './render-utils.js';
 const goblinBox = document.getElementById('goblin-box');
 const addGoblin = document.querySelector('[name="add-goblins"]');
+const combatLog = document.querySelector('#combat-log');
 
 // let state
 
@@ -11,28 +10,7 @@ const addGoblin = document.querySelector('[name="add-goblins"]');
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
-function displayGoblins() {
-    goblinBox.textContent = '';
-    for (let goblin of goblins){
-        const goblinEL = document.createElement('div');
-        const goblinName = document.createElement('h3');
-        const goblinHealth = document.createElement('p');
-    
-        goblinName.textContent = goblin.name;
-        goblinHealth.textContent = goblin.health;
-    
-        goblinEL.classList.add('goblin');
-        goblinEL.append(goblinName, goblinHealth);
-        goblinBox.append(goblinEL);
-        
-        goblinEL.addEventListener('click', () => {
-            goblin.health--;
-            displayGoblins();
-        });
-    }
 
-    
-}
 
 displayGoblins();
 
@@ -46,3 +24,43 @@ addGoblin.addEventListener('submit', (e) => {
     goblins.push(newGoblin);
     displayGoblins();
 });
+
+function displayGoblins() {
+    goblinBox.textContent = '';
+    for (let goblin of goblins){
+        const goblinEL = document.createElement('div');
+        const goblinName = document.createElement('h3');
+        const goblinHealth = document.createElement('p');
+        goblinName.textContent = goblin.name;
+        goblinHealth.textContent = goblin.health;
+        goblinEL.classList.add('goblin');
+        goblinEL.append(goblinName, goblinHealth);
+        goblinBox.append(goblinEL);
+        
+        goblinEL.addEventListener('click', () => {
+            attackRoll();
+            if (attackRoll() >= 4.9) {
+                goblin.health--;
+                addCombatLogHit(combatLog, goblin);
+                displayGoblins();
+            } else {
+                addCombatLogMiss(combatLog, goblin);
+            }
+        });
+        
+    }
+}
+function addCombatLogHit(listEl, target) {
+    const logItem = document.createElement('li');
+    logItem.textContent = `You hit ${target.name}`;
+    listEl.append(logItem);
+}
+function addCombatLogMiss(listEl, target){
+    const logItem = document.createElement('li');
+    logItem.textContent = `You missed ${target.name}`;
+    listEl.append(logItem);
+}
+function attackRoll(){
+    const roll = Math.floor(Math.random() * 10);
+    return roll;
+}
